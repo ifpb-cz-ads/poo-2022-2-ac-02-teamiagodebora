4e2e70081eba0e2c3805d31a4256111b176c17fc
IagoGOOl <iago.gool@gmail.com>
2022-10-13 18:49:54 -0300

    resolunção do exercicio parte 1

---
+++ Atividade 3/BMExercicio03.java
@@ -0,0 +1,24 @@
+
+
+public class BMExercicio03 {
+    public static void main(String[] args) {
+        int i;
+        long l;
+        float f = 40.f;
+        double d = 10.0;
+        boolean b;
+        char c;
+        i = 5;
+        l = 6;
+        b = true;
+        c = 'i';
+
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
